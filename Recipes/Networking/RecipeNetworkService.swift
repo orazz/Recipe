f449690ca36a055c0628d724754560b382dad1f6
Oraz <orazz.tm@gmail.com>
2024-10-06 16:14:42 -0500

    Remove comments

--- Recipes/Networking/RecipeNetworkService.swift
+++ Recipes/Networking/RecipeNetworkService.swift
@@ -38,14 +38,9 @@ class ImageCache: IImageCache {
 // MARK: - RecipeNetworkService
 
 struct RecipeNetworkService: IRecipeNetworkService {
-//    private let cache: IImageCache
     
     // MARK: - Proivate properties
     private let recipeNetworkService = NetworkService(session: URLSession.shared, baseURL: NetworkEndpoints.baseURL)
-
-//    init(cache: IImageCache = ImageCache()) {
-//        self.cache = cache
-//    }
 
     private let imageCache: CompositedImageCache
 
@@ -76,18 +71,5 @@ struct RecipeNetworkService: IRecipeNetworkService {
         imageCache.setImage(image, for: key)
 
         return image
-//        let requestLoadImage = NetworkRequestLoadImage(urlString)
-//        if let cachedImage = cache.getImage(for: urlString) {
-//            return cachedImage
-//        }
-//
-//        let imageData: Data = try await recipeNetworkService.perform(requestLoadImage)
-//
-//        guard let image = UIImage(data: imageData) else {
-//            throw HTTPNetworkServiceError.invalidResponse(nil)
-//        }
-//
-//        cache.setImage(image, for: urlString)
-//        return image
     }
 }
